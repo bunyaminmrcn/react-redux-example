9651866cb8382d5e7bbd89a75c26b9a3332e700b
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-11-19 14:58:12 +0300

    first enzyme test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,14 +3,22 @@ import React from "react";
 import '@testing-library/jest-dom'
 
 import App from "../App";
+import CommentBox from "../CommentBox";
+
 import { screen, render } from '@testing-library/react';
+
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
 
 it('shows a comment box', ()=> {
     
-    render(<App />)
-    const linkElement = screen.getByText(/Comment Box/i)
-    expect(linkElement).toBeInTheDocument();
+    //render(<App />)
+    //const linkElement = screen.getByText(/Comment Box/i)
+    //expect(linkElement).toBeInTheDocument();
 
 
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(CommentBox).length).toEqual(1)
 
 })
