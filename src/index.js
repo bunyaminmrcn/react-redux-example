48ad21529c80201d7753b3562021e5f438532691
mrcnbunyamin <mrcnbunyamin@gmail.com>
2023-11-19 19:34:00 +0300

    redux setup and add webpack.config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit'
+
+import reducer from "./reducers";
+const store = configureStore({
+    // Automatically calls `combineReducers`
+    reducer: reducer
+})
 
 import App from "./components/App";
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />)
+root.render(<Provider store={store}><App /></Provider>)
