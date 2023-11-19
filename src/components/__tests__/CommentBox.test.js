5f07222370dd509c6ee7ba203d59f539283b5970
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-11-19 17:40:39 +0300

    simulate test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -15,9 +15,38 @@ afterEach(() => {
     wrapper.unmount();
 })
 it('has a text area and a button', ()=> {
-    ;
+    
     expect(wrapper.find('textarea').length).toEqual(1);
     expect(wrapper.find('button').length).toEqual(1);
     expect(wrapper.find('input').length).toEqual(0);
 
 })
+
+
+it('has a text area that users can type in', ()=> {
+    
+    wrapper.find('textarea').simulate('change', {
+        target: { value: 'new comment'}
+    });
+   
+    wrapper.update();
+    
+    expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
+})
+
+
+
+it('when form is submitted, text area gets emptied', ()=> {
+    
+    wrapper.find('textarea').simulate('change', {
+        target: { value: 'new comment'}
+    });
+   
+    wrapper.update();
+    expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
+    
+    wrapper.find('form').simulate('submit');
+    wrapper.update();
+    expect(wrapper.find('textarea').prop('value')).toEqual('')
+})
+
