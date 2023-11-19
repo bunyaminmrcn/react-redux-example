83832f885a6aff973700cf775b709e17bf054ef2
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-11-19 18:14:46 +0300

    jest describe usage

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -14,8 +14,8 @@ beforeEach(() => {
 afterEach(() => {
     wrapper.unmount();
 })
-it('has a text area and a button', ()=> {
-    
+it('has a text area and a button', () => {
+
     expect(wrapper.find('textarea').length).toEqual(1);
     expect(wrapper.find('button').length).toEqual(1);
     expect(wrapper.find('input').length).toEqual(0);
@@ -23,30 +23,32 @@ it('has a text area and a button', ()=> {
 })
 
 
-it('has a text area that users can type in', ()=> {
-    
-    wrapper.find('textarea').simulate('change', {
-        target: { value: 'new comment'}
-    });
-   
-    wrapper.update();
-    
-    expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
+
+describe('the text area', () => {
+
+
+    beforeEach(() => {
+        wrapper.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        });
+
+        wrapper.update();
+    })
+
+    it('has a text area that users can type in', () => {
+
+        expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
+    })
+
+
+
+    it('when form is submitted, text area gets emptied', () => {
+
+        expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
+
+        wrapper.find('form').simulate('submit');
+        wrapper.update();
+        expect(wrapper.find('textarea').prop('value')).toEqual('')
+    })
+
 })
-
-
-
-it('when form is submitted, text area gets emptied', ()=> {
-    
-    wrapper.find('textarea').simulate('change', {
-        target: { value: 'new comment'}
-    });
-   
-    wrapper.update();
-    expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
-    
-    wrapper.find('form').simulate('submit');
-    wrapper.update();
-    expect(wrapper.find('textarea').prop('value')).toEqual('')
-})
-
