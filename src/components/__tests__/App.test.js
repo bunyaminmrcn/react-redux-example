425670cbef103c5fbd944bb28cf82bf7fa4491e9
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-11-19 14:09:04 +0300

    write first test

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import { createRoot } from 'react-dom/client';
+import '@testing-library/jest-dom'
+
+import App from "../App";
+import { screen, render } from '@testing-library/react';
+
+it('shows a comment box', ()=> {
+    
+    render(<App />)
+    const linkElement = screen.getByText(/Comment Box/i)
+    expect(linkElement).toBeInTheDocument();
+
+})
